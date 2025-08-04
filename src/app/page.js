c316608d74aeb54bae1ec524c06ca765e211506d
BagusASND <gli-bagus-uiux@gli-Bagus-UiUxs-MacBook-Pro.local>
2025-08-04 10:43:24 +0700

    update pdf coming soon

--- src/app/page.js
+++ src/app/page.js
@@ -5,20 +5,18 @@ import "./assets/scss/main.scss";
 export default function Home() {
   return (
     <div>
-      <header style={{ position: "fixed", width: "100%", top: "0", left: "0", zIndex: "2" }}>
-        <div className="container py-4">
-          <img src="https://www.asset.uiuxindo.id/src/assets/main/Logo-PDF-2028.png" alt="" style={{ height: "63px" }} />
-        </div>
-      </header>
       <div style={{ position: "fixed", width: "100%", top: "0", left: "0", height: "100%", backgroundImage: "url('https://www.asset.uiuxindo.id/src/assets/main/Accent-bg-pdf-2028.png')", backgroundSize: "cover", backgroundPosition: "center"  }} className="d-flex align-items-center justify-content-center">
         <div className="container">
           <div className="row justify-content-center">
+            <div className="col-12 text-center pb-4">
+              <img src="https://www.asset.uiuxindo.id/src/assets/main/Logo-PDF-2028.png" alt="" style={{ height: "63px" }} />
+            </div>
             <div className="col-12 col-sm-12 col-md-6 text-center">
               <h2 className="mb-2" style={{ color: "#3587FC" }}>Jadilah yang pertama tahu!</h2>
               <p>Bergabunglah dengan waiting list ekslusif kami dan dapatkan akses early bird</p>
               <div className="row">
                 <div className="col-12 col-sm-12 col-md-6 text-md-end pb-3 pb-sm-3 pb-md-0">
-                  <a href="" target="_blank">
+                  <a href="http://bit.ly/wl-pdf2026" target="_blank">
                     <button type="button" className="btn btn-primary pill">
                       <div className="d-flex align-items-center gap-2">
                         <img src="https://www.asset.uiuxindo.id/src/assets/main/mdi_user-group.svg" />
