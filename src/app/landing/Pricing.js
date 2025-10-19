1bc2c20d4f2b2a3cfc764f6b1236026451f09dd2
ReaperZero28 <reynard.julian28@gmail.com>
2025-10-19 12:46:00 +0700

    update payment, pricing card

--- src/app/landing/Pricing.js
+++ src/app/landing/Pricing.js
@@ -2,9 +2,8 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Card } from "react-bootstrap";
-import "./landing.css";
 
-const cardDetail = {
+const commonData = {
   individu: {
     title: "Individu",
     link: "https://uiuxindo.myr.id/pl/pdf2026",
@@ -50,108 +49,88 @@ const PricingSection = () => {
       {/* Tab */}
       <div className="d-flex justify-content-center my-5">
         <div className="border p-2 rounded-pill">
-          {["presale1", "presale2", "normal"].map((tab, i) => {
-            const isDisabled = tab !== "presale1"; // hanya presale1 aktif
-            return (
-              <button
-                key={i}
-                onClick={() => !isDisabled && setActiveTab(tab)}
-                disabled={isDisabled}
-                className={`btn rounded-pill px-4 fw-500 ${
-                  activeTab === tab ? "tab-price-active" : "tab-price"
-                } ${i < 2 ? "me-1" : ""}`}
-                style={
-                  isDisabled
-                    ? {
-                        color: "#666",
-                        border: "none",
-                        cursor: "not-allowed",
-                      }
-                    : {}
-                }
-              >
-                {tab === "presale1"
-                  ? "Presale 1"
-                  : tab === "presale2"
-                  ? "Presale 2"
-                  : "Normal"}
-              </button>
-            );
-          })}
+          {["presale1", "presale2", "normal"].map((tab, i) => (
+            <button
+              key={i}
+              onClick={() => setActiveTab(tab)}
+              className={`btn rounded-pill px-4 fw-500 ${
+                activeTab === tab ? "tab-price-active" : "tab-price"} ${i < 2 ? "me-1" : ""}`}
+            >
+              {tab === "presale1" ? "Presale 1" : tab === "presale2" ? "Presale 2" : "Normal"}
+            </button>
+          ))}
         </div>
       </div>
 
       {/* Pricing Cards */}
       <div className="row mt-5 justify-content-center">
         {Object.keys(data).map((type, index) => {
-          const dataPrice = { ...cardDetail[type], ...data[type] };
+          const plan = { ...commonData[type], ...data[type] };
           const isBestOption =
-            activeTab === "presale1" && dataPrice.title === "Individu";
+            activeTab === "presale1" && plan.title === "Individu";
+
+          const isButtonDisabled =
+            activeTab === "presale2" || activeTab === "normal";
 
           return (
-            <div
-              className="col-md-6 col-12 position-relative mb-3 mb-md-0 px-3"
-              key={index}
-            >
+            <div className="col-md-6 col-12 position-relative mb-3 mb-md-0 px-3" key={index}>
               <Card
                 className={`${
-                  dataPrice.title === "Individu"
-                    ? "card-price-individu"
-                    : "card-price-group"
-                } h-100 px-md-4 px-3 py-4 mb-0 rounded-4`}
-                style={
-                  isBestOption ? { border: "2px solid #FFB800" } : {}
-                }
+                  plan.title === "Individu" ? "card-price-individu" : "card-price-group"} px-md-4 px-3 py-4 mb-0 rounded-4`}
+                style={isBestOption ? { border: "2px solid #FFB800" } : {}}
               >
-                <Card.Body className="h-100 d-flex flex-column align-items-center justify-content-center p-0">
+                <Card.Body className="d-flex flex-column align-items-center justify-content-center p-0">
                   <div className="w-100 d-flex justify-content-center align-items-center border-bottom pb-4">
                     <div className="text-main fw-700 fs-2xl me-3">
-                      {dataPrice.title}
+                      {plan.title}
                     </div>
                     {isBestOption && (
-                      <div className="d-flex align-items-center bg-warning text-dark rounded-pill px-3 py-2">
+                      <div className="d-flex align-items-center bg-warning text-dark rounded-pill px-3 py-2 fw-600" 
+                           style={{ fontSize: "14px" }}
+                      >
                         <i className="bi bi-hand-thumbs-up me-2"></i>
                         Opsi Terbaik
                       </div>
                     )}
                   </div>
 
-                  <div className="d-flex flex-md-row flex-column align-items-center justify-content-center my-md-4 card-content">
-                    <div className="pb-md-4 pb-3 pb-md-0 h-100 text-center pe-md-5 price">
-                      <h2
-                        className="text-main mt-md-5 mt-4 fw-700"
-                        style={{ fontSize: "32px" }}
-                      >
-                        {dataPrice.price}
+                  <div className="d-flex flex-md-row flex-column align-items-center justify-content-center my-4 card-content">
+                    <div className="pb-4 pb-md-0 h-100 price text-center pe-md-5">
+                      <h2 className="text-main mt-md-5 mt-4 fw-700" style={{ fontSize: "32px" }}>
+                        {plan.price}
                       </h2>
                       <p className="text-grey mb-0 fs-lg">
-                        {dataPrice.date || dataPrice.per}
+                        {plan.date || plan.per}
                       </p>
                     </div>
-
-                    <ul
-                      className="ps-0 ps-md-5 pe-4 pe-md-0 mb-md-0 mb-3 text-start"
-                      style={{ listStyleType: "none" }}
-                    >
-                      {dataPrice.benefit.map((item, i) => (
+                    <ul className="ps-0 ps-md-5 pe-4 pe-md-0 mb-0 text-start" style={{ listStyleType: "none" }}>
+                      {plan.benefit.map((item, i) => (
                         <li key={i} className="mt-2">
-                          <i
-                            className="bi bi-check-circle-fill me-2"
-                            style={{ color: "#22C55E" }}
-                          ></i>
+                          <i className="bi bi-check-circle-fill me-2" style={{ color: "#22C55E" }}></i>
                           {item}
                         </li>
                       ))}
                     </ul>
                   </div>
 
-                  {/* Button */}
-                  <Link
-                    href={dataPrice.link}
-                    className="btn btn-price rounded-pill w-100 fw-600"
-                  >
-                    Beli Tiket
-                  </Link>
+                  {isButtonDisabled ? (
+                    <button className="btn btn-price rounded-pill w-100 fw-600"
+                      disabled
+                      style={{
+                        opacity: 0.6,
+                        cursor: "not-allowed",
+                        backgroundColor: "#C7C7C7",
+                        color: "#666",
+                        border: "none",
+                      }}
+                    >
+                      Beli Tiket
+                    </button>
+                  ) : (
+                    <Link href={plan.link} className="btn btn-price rounded-pill w-100 fw-600">
+                      Beli Tiket
+                    </Link>
+                  )}
                 </Card.Body>
               </Card>
             </div>
